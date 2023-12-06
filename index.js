98b6dd2d80386816b7963044e3cebb94c8ce5ce0
Vlad Phokhnitskyi <66656318+Nansokolov@users.noreply.github.com>
2023-12-06 09:21:47 -0500

    portfolio

--- index.js
+++ index.js
@@ -1,12 +1,15 @@
 function scrollFunc(currentEl, toElemId){
+    setClassActive(currentEl);
+    const body = document.querySelector("body");
+    const toelemHeight = document.getElementById(toElemId).offsetTop;
+    body.scrollTo({top:toelemHeight,behavior:'smooth'});
+}
+
+function setClassActive(elem){
     let allActive = document.getElementsByClassName("active");
     for (const element of allActive){
         element.classList.remove("active")
     }
-    currentEl.classList.add("active");
-    const body = document.querySelector("body");
-    const toelemHeight = document.getElementById(toElemId).offsetTop;
-    console.log(toElemId)
-    console.log(toelemHeight)
-    body.scrollTo({top:toelemHeight,behavior:'smooth'});
+    elem.classList.add("active");
+
 }
